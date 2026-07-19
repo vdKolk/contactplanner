e9f3b1f029929c1bbbcf0db4a398f8f0306889af
vdKolk <ruben.van.der.kolk@woodwing.com>
2026-07-19 23:19:25 +0200

    Add local encryption

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
      offline blijft werken.
    Verhoog CACHE_VERSIE bij elke wijziging aan de app-bestanden. */
 
-const CACHE_VERSIE = "contactplanner-v1";
+const CACHE_VERSIE = "contactplanner-v2";
 const KERN_BESTANDEN = [
   "./",
   "index.html",
